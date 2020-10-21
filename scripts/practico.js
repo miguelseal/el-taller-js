546647dbe7cbfdf3529a7d92a919d3a6ad3eeaa9
Fernando Miguel <fmiguelkoczwar@gmail.com>
2020-10-20 21:28:28 -0300

    Practico 2 - hasta ej. 5

--- scripts/practico.js
+++ scripts/practico.js
@@ -1,4 +1,10 @@
+
+/*******************************************
+ * PRACTICO 1
+ *******************************************/
+
 // Ejercicio 1
+document.write("<h2>Practico 1</h2>");
 alert("Hola Mundo desde js externo");
 
 // Ejercicio 2
@@ -36,3 +42,67 @@ var age = 38;
 
 document.write("<p>Hola, me llamo ", name, ", tengo ", age, " años.</p><hr>");
 document.write("<p>Me gusta estudiar <strong>programación</strong>.</p>");
+
+
+/*******************************************
+ * PRACTICO 2
+ *******************************************/
+
+//Ejercicio 1 (area del circulo)
+document.write("<h2>Practico 2 - Ej 1.</h2>");
+
+const PI = 3.14;
+const RADIUS = 10 * 100; // convierto a cm.
+
+// area = r^2 * PI
+var area = RADIUS ** 2 * PI;
+
+// Resultados:
+document.write("<p>El area de un circulo de 10 mts de radio es: ", area, " cm.^2.</p>");
+
+
+//Ejercicio 2 (suma de dos valores ingresados por el usuario)
+
+document.write("<h2>Practico 2 - Ej 2.</h2>");
+
+var num1 = parseInt(prompt("Escribe un numerito"));
+var num2 = parseInt(prompt("Escribe otro numerito"));
+
+// Calculando la suma y mostrando resultados en el doc.
+var sum = num1 + num2;
+document.write("<p>La suma de los dos numeros ingresados por Usted es: ", sum, ".</p>");
+
+
+//Ejercicio 3 (Que numero es mayor?)
+document.write("<h2>Practico 2 - Ej 3.</h2>");
+
+var num1 = parseInt(prompt("Numero 1"));
+var num2 = parseInt(prompt("Numero 2"));
+
+if (num1 >= num2) {
+    document.write("<p>El primer numero es <strong>mayor</strong> (o igual) que el segundo.</p>");
+} else {
+    document.write("<p>El segundo numero que ingresaste es <strong>mayor</strong>.</p>");
+}
+
+
+// Ejercicio 4 (Mayor de 3 numeros)
+document.write("<h2>Practico 2 - Ej 4.</h2>");
+var msg = "Ingresa un entero.";
+var num, greatest;
+
+num = parseInt(prompt(msg));
+greatest = num;
+
+for (var i = 0; i < 2; i++) {
+    num = parseInt(prompt(msg));
+
+    num > greatest ? greatest = num : num = undefined;
+}
+
+document.write("<p>El mayor numero de los 3 es: ", greatest, ".</p>");
+
+//Ejercicio 5 (Divisible por 2).
+document.write("<h2>Practico 2 - Ej 5.</h2>");
+
+
